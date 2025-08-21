78e2f777c9a1e44395eeeeaea844087ffe80cbbd
JulianFP <julian@partanengroup.de>
2025-08-21 18:41:05 +0200

    Add sms_spam dataset with different feature sizes to compare performance

--- src/concrete_ml_playground/dataset_collector.py
+++ src/concrete_ml_playground/dataset_collector.py
@@ -5,5 +5,12 @@ from .datasets.synthetic import load_synthetic_dataset
 def get_dataset_loaders():
     return {
         "synthetic": load_synthetic_dataset,
-        "sms_spam": load_sms_spam_dataset,
+        "sms_spam_all_features": load_sms_spam_dataset,
+        "sms_spam_5000_features": lambda: load_sms_spam_dataset(500),
+        "sms_spam_2500_features": lambda: load_sms_spam_dataset(500),
+        "sms_spam_1000_features": lambda: load_sms_spam_dataset(500),
+        "sms_spam_500_features": lambda: load_sms_spam_dataset(500),
+        "sms_spam_250_features": lambda: load_sms_spam_dataset(250),
+        "sms_spam_100_features": lambda: load_sms_spam_dataset(100),
+        "sms_spam_50_features": lambda: load_sms_spam_dataset(50),
     }
--- src/concrete_ml_playground/datasets/sms_spam.py
+++ src/concrete_ml_playground/datasets/sms_spam.py
@@ -4,11 +4,11 @@ from datasets import load_dataset
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-def load_sms_spam_dataset() -> tuple[npt.NDArray, npt.NDArray]:
+def load_sms_spam_dataset(max_features: None | int = None) -> tuple[npt.NDArray, npt.NDArray]:
     dataset = load_dataset("ucirvine/sms_spam", split="train")
     dataset = dataset.with_format("numpy")
 
-    vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5, min_df=5, stop_words="english")
+    vectorizer = TfidfVectorizer(stop_words="english", max_features=max_features)
     X = vectorizer.fit_transform(dataset["sms"])
 
     return X.toarray(), np.array(dataset["label"])
